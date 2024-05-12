0d79b4efb2bb07a9417c6b82b3c15c72cb62d2ae
Subayyal Ismail Saeed <127910222+SaeedSubayyal@users.noreply.github.com>
2024-05-13 02:08:44 +0500

    Add files via upload

---
+++ assets/icons/arraowLeft.js
@@ -0,0 +1,20 @@
+export default color => `
+<svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_165_857)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12.408 5.92548C12.7253 6.24278 12.7253 6.75722 12.408 7.07452L7.29504 12.1875H22.6668C23.1156 12.1875 23.4793 12.5513 23.4793 13C23.4793 13.4487 23.1156 13.8125 22.6668 13.8125H7.29504L12.408 18.9255C12.7253 19.2428 12.7253 19.7572 12.408 20.0745C12.0907 20.3918 11.5763 20.3918 11.259 20.0745L4.75897 13.5745C4.44167 13.2572 4.44167 12.7428 4.75897 12.4255L11.259 5.92548C11.5763 5.60817 12.0907 5.60817 12.408 5.92548Z" fill="#BABDC1"/>
+</g>
+<defs>
+<filter id="filter0_d_165_857" x="-3" y="0" width="34" height="34" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_165_857"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_165_857" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+`
